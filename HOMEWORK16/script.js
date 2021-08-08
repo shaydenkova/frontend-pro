be1c669ff40aef0b5c6c09a3cd40ea40509ca41f
shaydenkova <82531349+shaydenkova@users.noreply.github.com>
2021-08-08 16:49:55 +0300

    Add files via upload

---
+++ HOMEWORK16/script.js
@@ -0,0 +1,70 @@
+//1 задача
+
+const personJohn = {
+    name: "John",
+    sayHello: function() {
+      console.log(`Hello, I'm ${this.name}`);
+    }
+  }
+  
+  const sysAdmin = {
+    name: "Bob",
+    __proto__: personJohn
+  }
+  
+  const clientNatalia = {
+    name: "Natalia",
+    __proto__: personJohn
+  }
+
+sysAdmin.sayHello();
+clientNatalia.sayHello();Ad
+
+
+//2 задача
+
+function Person(name) {
+    this.name = name;
+    this.sayHello = function() {
+      console.log(`Hello, I'm ${this.name}`);
+    }
+  }
+  
+function Employee(name) {
+    this.name = name;
+}
+  
+function Client(name) {
+    this.name = name;
+}
+  
+const personJohn = new Person("John");
+const sysAdmin = new Employee("Bob");
+sysAdmin.__proto__ = personJohn;
+sysAdmin.sayHello();
+const clientNatalia = new Client("Natalia");
+clientNatalia.__proto__ = personJohn;
+clientNatalia.sayHello();
+
+//3 задача
+ 
+function Student(name, markArray){
+    this.name = name;
+    this.markArray = markArray;
+    this.averageMark = function() {
+        let totalAmount = this.markArray.reduce((total, amount) => total + amount)/this.markArray.length; 
+        console.log(Math.round(totalAmount))
+      }
+}
+
+const students = [ 
+    new Student('Student 1', [10,9,8,0,10]), // имя, оценки
+    new Student('Student 12', [10,0,8,0,3,4])
+];
+
+students[0].averageMark();
+students[1].averageMark();
+
+
+
+
