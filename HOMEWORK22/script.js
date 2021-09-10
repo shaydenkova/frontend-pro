eb4288d150f94f9530a17884f52dac0733e3807a
shaydenkova <82531349+shaydenkova@users.noreply.github.com>
2021-09-10 15:40:27 +0300

    Add files via upload

--- HOMEWORK22/script.js
+++ HOMEWORK22/script.js
@@ -4,15 +4,18 @@
  	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b6b679a1b41a7204be8734c7dd3160e6`)  
   	.then(resp => resp.json())
   	.then(data => {
-  		let cloudsValue = data.weather[0].main;
-  		let tempValue = Math.round(+data.main.temp - 273) + " ℃";
-  		let windValue = data.wind.speed + " km/h";
-  		let humidityValue = data.main.humidity + " %";
-  		let pressureValue = data.main.pressure + " mm";
-  		postValues(cityName, cloudsValue, tempValue, windValue, humidityValue, pressureValue);
- 	});
- 	let sectionWeather = document.getElementById("sectionWeather");
- 	sectionWeather.classList.remove("hidden");
+  		if(data.name) {
+  			let sectionWeather = document.getElementById("sectionWeather");
+ 			sectionWeather.classList.remove("hidden");
+	  		let cloudsValue = data.weather[0].main;
+	  		let tempValue = Math.round(+data.main.temp - 273) + " ℃";
+	  		let windValue = data.wind.speed + " km/h";
+	  		let humidityValue = data.main.humidity + " %";
+	  		let pressureValue = data.main.pressure + " mm";
+	  		postValues(cityName, cloudsValue, tempValue, windValue, humidityValue, pressureValue);
+  		} else {
+  			alert("The name of city is wrong. Please enter correct name!");
+  		}})
  }
 
  function postValues(val1, val2, val3, val4, val5, val6) {
