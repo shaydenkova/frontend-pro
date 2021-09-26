59a7d8e0f90b6f767fb18c72f3c9220eb3f1abdc
shaydenkova <82531349+shaydenkova@users.noreply.github.com>
2021-09-26 17:27:24 +0300

    Add files via upload

---
+++ HOMEWORK26/app.js
@@ -0,0 +1,6 @@
+import * as calculator from './calculator.js';
+
+console.log(calculator.add(10, 2)) // 12
+console.log(calculator.sub(12, 5)) // 7nodnode
+console.log(calculator.mult(7, 4)) // 28
+console.log(calculator.div(28, 2)) // 14nod
---
+++ HOMEWORK26/calculator.js
@@ -0,0 +1,12 @@
+import {add} from './add.js';
+export {add};
+import {sub} from './sub.js';
+export {sub};
+import {mult} from './mult.js';
+export {mult};
+import {div} from './div.js';
+export {div};
+
+
+
+
